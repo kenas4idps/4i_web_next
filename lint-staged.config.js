dd15ee19c5fb6579b164d80a745c5a16ca93c486
kenas4idps <kenas@4idps.com>
2025-02-06 16:27:52 +0800

    chore(eslint-staged): added

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{js,jsx,mjs,ts,tsx,mts,mdx}': ['prettier --ignore-path .prettierignore --write'],
+  '*.{json,md,css,html,yml,yaml,scss}': ['prettier --ignore-path .prettierignore --write'],
+};
