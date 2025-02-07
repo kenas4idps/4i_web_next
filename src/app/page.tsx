3b7af9ab572b1b9f7bfb28d33dd112e30b863f4e
kenas4idps <kenas@4idps.com>
2025-02-07 11:06:26 +0800

    chore(i18n): added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client';
+
+import { Button } from '@/components/ui/Button';
+import { useLocale, useTranslations } from 'next-intl';
 import Image from 'next/image';
+import { setUserLocale } from '../services/locale';
 
 export default function Home() {
+  const t = useTranslations('HomePage');
+  const locale = useLocale();
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
@@ -21,7 +29,17 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>HomePage translation: {t('title')}</li>
         </ol>
+
+        <Button
+          onClick={async () => {
+            if (locale === 'de') await setUserLocale('en');
+            if (locale === 'en') await setUserLocale('de');
+          }}
+        >
+          Change locale to: {locale}
+        </Button>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
