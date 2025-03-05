a3b2f10686f8b01fbd1e2482ab0508e086bb017d
kenas4idps <kenas@4idps.com>
2025-03-05 18:26:45 +0800

    chore: relative path of fs read file

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,7 @@
 import { getRequestConfig } from 'next-intl/server';
 import { getUserLocale } from '../services/locale';
 import fs from 'fs';
+import path from 'path';
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
@@ -9,12 +10,12 @@ export default getRequestConfig(async () => {
 
   const messages: { [key: string]: any } = {};
 
-  const __dirname = `messages/${locale}/`;
+  const __dirname = path.join(process.cwd(), `messages/${locale}/`);
   const filenames = fs.readdirSync(__dirname);
 
-  filenames.forEach(async (file: any) => {
+  filenames.forEach((file: any) => {
     const name = file.split('.')[0];
-    messages[name] = JSON.parse(fs.readFileSync(`${__dirname}${file}`, 'utf8'));
+    messages[name] = JSON.parse(fs.readFileSync(path.join(__dirname, file), 'utf8'));
   });
 
   return {
