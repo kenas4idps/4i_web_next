0a28182b71e34f60fcd48e8def20ccf054e2a5c4
kenas4idps <kenas@4idps.com>
2025-03-06 22:01:04 +0800

    fix: logo and manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,6 +11,24 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
   return {
     name: t('name'),
     start_url: '/',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'favicon.ico',
+        sizes: '64x64 32x32 24x24 16x16',
+        type: 'image/x-icon',
+      },
+      {
+        src: 'logo192.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      {
+        src: 'logo512.png',
+        type: 'image/png',
+        sizes: '512x512',
+      },
+    ],
     theme_color: '#101E33',
   };
 }
